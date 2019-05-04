208cf4fa7abf87c7fb0147a611a42a8c5db58c9c
Marton <martondeveloper@gmail.com>
2019-05-04 13:42:50 +0200

    Movements, collisions, sizes, etc

--- app.js
+++ app.js
@@ -46,7 +46,10 @@ io.sockets.on("connection", function(socket) {
         socketID: socket.id,
         username: userinfo.username,
         x: randomCords.x,
-        y: randomCords.y
+        y: randomCords.y,
+        speed: 2.5,
+        size: Math.floor(Math.random() * 15) + 10,
+        alive: true
 
       });
 
@@ -55,7 +58,9 @@ io.sockets.on("connection", function(socket) {
       response({
 
         x: randomCords.x,
-        y: randomCords.y
+        y: randomCords.y,
+        id: playerID,
+        size: players[playerID].size
 
       });
 
@@ -67,19 +72,14 @@ io.sockets.on("connection", function(socket) {
 
   });
 
-  setInterval(function() {
-
-    socket.emit("players", players);
-
-  }, 1000 / 60);
-
-  socket.on("disconnect", function() {
+  socket.on("tp", function(data) {
 
     for(var i = 0; i < players.length; i++) {
 
-      if(players[i].socketID == socket.id) {
+      if(players[playerID].alive) {
 
-        players.splice(i, 1);
+        players[i].x = data.x;
+        players[i].y = data.y;
 
       }
 
@@ -87,28 +87,89 @@ io.sockets.on("connection", function(socket) {
 
   });
 
+  socket.on("disconnect", function() {
+
+    players[playerID].alive = false;
+
+  });
+
   socket.on("mouse", function(mouse) {
 
-    if(playerID === undefined) {
+    if(playerID === undefined || players[playerID]  === undefined || !players[playerID].alive) {
 
       return;
 
     }
 
-    players[playerID].x += mouse.x;
-    players[playerID].y += mouse.y;
+    var percentage = (players[playerID].speed / Math.sqrt((mouse.x * mouse.x) + (mouse.y * mouse.y))) * 100;
 
-    console.log("New pos: " + players[playerID].x);
+    players[playerID].x += (mouse.x / 100) * percentage;
+    players[playerID].y += (mouse.y / 100) * percentage;
 
   });
 
 });
 
+setInterval(function() {
+
+  io.emit("players", players);
+
+  for(var i = 0; i < players.length; i++) {
+
+    if(players[i].alive) {
+
+      if(players[i].x < 0 || players[i].x > 10000 || players[i].y < 0 || players[i].y > 10000) {
+
+        io.emit("death", i);
+        players[i].alive = false;
+
+      }else {
+
+        for(var j = 0; j < players.length; j++) {
+
+          if(j != i && players[j].alive && isColliding(players[i].x, players[i].y, players[i].size, players[j].x, players[j].y, players[j].size)) {
+
+            if(players[i].size == players[j].size) {
+
+              players[i].alive = false;
+              io.emit("death", i);
+              players[j].alive = false;
+              io.emit("death", j);
+
+            }else {
+
+              var collidingItems = {
+
+                smaller: players[i].size > players[j].size ? j : i,
+                bigger: players[i].size > players[j].size ? i : j
+
+              };
+
+              io.emit("death", collidingItems.smaller);
+              players[collidingItems.smaller].alive = false;
+              players[collidingItems.bigger].size += players[collidingItems.smaller].size;
+
+            }
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+}, 1000 / 60);
+
+setInterval();
+
 function playerExists(username) {
 
   for(var i = 0; i < players.length; i++) {
 
-    if(players[i].username == username) {
+    if(players[i].username == username && players[i].alive) {
 
       return true;
 
@@ -117,5 +178,27 @@ function playerExists(username) {
   }
 
   return false;
+
+}
+
+function isColliding(p1x, p1y, r1, p2x, p2y, r2) {
+
+  var a;
+  var x;
+  var y;
+
+  a = r1 + r2;
+  x = p1x - p2x;
+  y = p1y - p2y;
+
+  if(a > Math.sqrt((x * x) + (y * y))) {
+
+    return true;
+
+  }else {
+
+    return false;
+
+  }
 
 }
--- main.js
+++ main.js
@@ -10,14 +10,14 @@ socket.on("packetToClient", function(data) {
 
 var username = "";
 var mouse = {x: 0, y: 1};
-var player = {x: 0, y: 0};
+var player = {x: 0, y: 0, alive: false, id: 0, size: 0};
 
 jQuery(function($) {
 
   $(document).ready(function() {
 
     $(window).trigger("resize");
-    drawPlayer($(window).width() / 2, $(window).height() / 2, "#ff0000");
+    drawPlayer($(window).width() / 2, $(window).height() / 2, "#ff0000", 15, "You");
 
   });
 
@@ -51,6 +51,9 @@ jQuery(function($) {
         username = requestUsername;
         player.x = response.x;
         player.y = response.y;
+        player.alive = true;
+        player.id = response.id;
+        player.size = response.size;
 
       }else {
 
@@ -71,7 +74,7 @@ jQuery(function($) {
 
     });
 
-  }, 1000 / 55);
+  }, 1000 / 60);
 
   socket.on("players", function(players) {
 
@@ -80,42 +83,96 @@ jQuery(function($) {
 
     for(var i = 0; i < players.length; i++) {
 
+      if(typeof players[i] === "undefined" || players[i] == null || !players[i].alive) {
+
+        continue;
+
+      }
+
       if(players[i].username != username) {
 
-        drawPlayer($(gameCanvas).attr("width") / 2 - player.x + players[i].x, $(gameCanvas).attr("height") / 2 - player.y + players[i].y, "#000");
+        drawPlayer($(gameCanvas).attr("width") / 2 - player.x + players[i].x, $(gameCanvas).attr("height") / 2 - player.y + players[i].y, "#000", players[i].size, players[i].username);
 
-      }else {
+      }else if(player.alive) {
 
         player.x = players[i].x;
         player.y = players[i].y;
+        player.id = i;
+        player.size = players[i].size;
 
       }
 
     }
 
-    drawPlayer($(window).width() / 2, $(window).height() / 2, "#ff0000");
+    if(player.alive) {
+
+      drawPlayer($(window).width() / 2, $(window).height() / 2, "#ff0000", players[player.id].size, players[player.id].username);
+
+      gameContent.fillStyle = "#000";
+      gameContent.font = "14px Arial";
+      gameContent.fillText("X: " + Math.floor(player.x) + " Y: " + Math.floor(player.y), $(window).width() - (gameContent.measureText("X: " + Math.floor(player.x) + " Y: " + Math.floor(player.y)).width) - 5, 16);
+      gameContent.fillText("Score: " + player.size, $(window).width() - (gameContent.measureText("Score: " + player.size).width) - 5, 32);
+
+    }
+
+  });
+
+  socket.on("death", function(deathID) {
+
+    if(player.id != deathID) {
+
+      return;
+
+    }
+
+    if(document.getElementsByClassName("join-modal").length != 0) {
+
+      return;
+
+    }
+
+    $("body").append('<div class="join-modal"><div class="container">You died! Rejoin the game:<br><input type="text" placeholder="Name"><button>Join!</button><p class="error"></p></div></div>');
+    player.alive = false;
 
   });
 
 });
 
-function drawPlayer(x, y, color) {
+function drawPlayer(x, y, color, size, name) {
 
   gameContent.beginPath();
-  gameContent.arc(x, y, 10, 0, 2 * Math.PI);
+  gameContent.arc(x, y, size, 0, 2 * Math.PI);
   gameContent.fillStyle = color;
   gameContent.fill();
+  gameContent.fillStyle = color == "#000" ? "#fff" : "#000";
+  gameContent.font = "10px Arial";
+  gameContent.fillText(name, x - (gameContent.measureText(name).width / 2), y + 5);
 
 }
 
 function drawCourt(x, y) {
 
-  for(var i = 0; i < (10000 / 20); i++) {
+  for(var i = 0; i < (10020 / 20); i++) {
 
     gameContent.fillStyle = "#b7b7b7";
     gameContent.fillRect(x, y + i * 20, 10000, 1);
     gameContent.fillRect(x + i * 20, y, 1, 10000);
 
   }
+
+  gameContent.fillStyle = "#b7b7b7";
+  gameContent.fillRect(9999, 0, 1, 10000);
+  gameContent.fillRect(0, 9999, 10000, 1);
+
+}
+
+function tp(x, y) {
+
+  socket.emit("tp", {
+
+    x: x,
+    y: y
+
+  });
 
 }
